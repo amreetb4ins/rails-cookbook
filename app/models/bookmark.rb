a72333fe6f83aeaaf0e61dc5d97bef1481bcc3c3
Amreet Bains <amreet8@hotmail.co.uk>
2024-09-30 10:32:44 +0100

    added validations for models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
 
+  validates :recipe_id, uniqueness: { scope: :category_id, message: "is already bookmarked in this category" }
   validates :comment, length: { minimum: 6 }
 end
