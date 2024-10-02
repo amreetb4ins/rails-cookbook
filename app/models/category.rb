9b4110ff17d680f310c9af2f94d56c245cac7b36
Amreet Bains <amreet8@hotmail.co.uk>
2024-10-02 10:29:30 +0100

    cloudinary images, updated category model and controller and index

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :recipes, through: :bookmarks
+  has_one_attached :photo
+
 
   validates :name, uniqueness: true, presence: true
 end
